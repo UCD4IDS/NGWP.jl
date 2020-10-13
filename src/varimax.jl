e5fa755bb1339e2c1e8fc84fecfe2d6acfcd9a14
Haotian Li <htsli@ucdavis.edu>
2020-10-13 11:04:10 -0700

    indentation

--- src/varimax.jl
+++ src/varimax.jl
@@ -30,28 +30,28 @@ function varimax(A; gamma = 1.0, minit = 20, maxit = 1000, reltol = 1e-12)
 	L,_,M = svd(A' * (d*B.^3 - gamma*B * Diagonal(sum(B.^2, dims = 1)[:])))
 	T = L * M'
 	if norm(T-Matrix{Float64}(I, m, m)) < reltol
-    	T,_ = qr(randn(m,m)).Q
-    	B = A * T
+		T,_ = qr(randn(m,m)).Q
+		B = A * T
 	end
 
 	# Iteration step: get better T to maximize the objective (as described in Factor Analysis book)
 	D = 0
 	for k in 1:maxit
-    	Dold = D
-    	L,s,M = svd(A' * (d*B.^3 - gamma*B * Diagonal(sum(B.^2, dims = 1)[:])))
-    	T = L * M'
-    	D = sum(s)
+		Dold = D
+		L,s,M = svd(A' * (d*B.^3 - gamma*B * Diagonal(sum(B.^2, dims = 1)[:])))
+		T = L * M'
+		D = sum(s)
 		B = A * T
-    	if (abs(D - Dold)/D < reltol) && k >= minit
-        	break
-    	end
+		if (abs(D - Dold)/D < reltol) && k >= minit
+			break
+		end
 	end
 
 	# Adjust the sign of each rotated vector such that the maximum absolute value is positive.
 	for i in 1:m
 		if abs(maximum(B[:,i])) < abs(minimum(B[:,i]))
-	    	B[:,i] .= - B[:,i]
-	   	end
+			B[:,i] .= - B[:,i]
+		end
 	end
 
 	return B
