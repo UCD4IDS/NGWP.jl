806dc44d2af3d731da04923ae940ea4d8b63bca4
haotian127 <htsli@ucdavis.edu>
2021-01-16 17:26:54 -0800

    update paper scripts for the final revision

--- test/paperscript/Sunflower_Barbara_approx_exp.jl
+++ test/paperscript/Sunflower_Barbara_approx_exp.jl
@@ -1,61 +1,58 @@
 ## Set up environment and load required packages
-using Pkg
-Pkg.activate(".")
-
 using NGWP, JLD, MAT, Plots, LightGraphs, MTSG
 gr(dpi = 200)
 
 ## Build weighted sunflower graph
 G, L, X = SunFlowerGraph(N = 400); N = nv(G)
-lamb, 𝛷 = eigen(Matrix(L)); sgn = (maximum(𝛷, dims = 1)[:] .> -minimum(𝛷, dims = 1)[:]) .* 2 .- 1; 𝛷 = Matrix((𝛷' .* sgn)')
+𝛌, 𝚽 = eigen(Matrix(L)); sgn = (maximum(𝚽, dims = 1)[:] .> -minimum(𝚽, dims = 1)[:]) .* 2 .- 1; 𝚽 = 𝚽 * Diagonal(sgn);
 Q = incidence_matrix(G; oriented = true)
 W = 1.0 * adjacency_matrix(G)
 edge_weight = [e.weight for e in edges(G)]
 
 ## Build Dual Graph by DAG metric
-distDAG = eigDAG_Distance(𝛷,Q,N; edge_weight = edge_weight)
+distDAG = eigDAG_Distance(𝚽,Q,N; edge_weight = edge_weight)
 W_dual = sparse(dualGraph(distDAG)) #required: sparse dual weighted adjacence matrix
 
-## Assemble natural graph wavelet packets
-ht_elist_dual, ht_vlist_dual = HTree_EVlist(𝛷,W_dual)
-wavelet_packet_dual = HTree_wavelet_packet(𝛷,ht_vlist_dual,ht_elist_dual)
-ht_elist_varimax = ht_elist_dual
-wavelet_packet_varimax = HTree_wavelet_packet_varimax(𝛷,ht_elist_varimax)
+## Assemble wavelet packets
+ht_elist_PC, ht_vlist_PC = HTree_EVlist(𝚽,W_dual)
+wavelet_packet_PC = HTree_wavelet_packet(𝚽,ht_vlist_PC,ht_elist_PC)
+ht_elist_VM = ht_elist_PC
+wavelet_packet_VM = HTree_wavelet_packet_varimax(𝚽,ht_elist_VM)
 
-## Fig. 4(b) barbara eye graph signal
-f = matread(joinpath(@__DIR__, "..", "datasets", "sunflower_barbara.mat"))["f_eye"]
-scatter_gplot(X; marker = f, ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); signal_plt = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], yflip = true, frame = :none)
-savefig(signal_plt, "test/paperfigs/SunFlower_barbara_feye.png")
-
-## Fig. 4(c) barbara eye relative l2 approximation error by various methods
-DVEC = signal_transform_coeff(f, ht_elist_dual, ht_elist_varimax, wavelet_packet_dual, wavelet_packet_varimax, 𝛷, W, X)
-approx_error_plot2(DVEC); plot!(legend = :topright); approx_error_plt = current()
-savefig(approx_error_plt, "test/paperfigs/SunFlower_barbara_feye_DAG_approx.png")
-
-## Fig. 5 barbara eye 9 most important VM-NGWP vectors (ignore the DC vector)
-parent_varimax = HTree_findParent(ht_elist_varimax); Wav_varimax = best_basis_selection(f, wavelet_packet_varimax, parent_varimax); dvec_varimax = Wav_varimax' * f
-importance_idx = sortperm(abs.(dvec_varimax), rev = true)
-for i = 2:10
-    scatter_gplot(X; marker = Wav_varimax[:,importance_idx[i]], ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); important_NGW_basis_vectors = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], yflip = true, frame = :none)
-    savefig(important_NGW_basis_vectors, "test/paperfigs/SunFlower_barbara_feye_DAG_VM_NGW_important_basis_vector$(i).png")
-end
-
-## Fig. 6(b) barbara pant graph signal
-f = matread(joinpath(@__DIR__, "..", "datasets", "sunflower_barbara.mat"))["f_trouser"]
-scatter_gplot(X; marker = f, ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); signal_plt = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], yflip = true, frame = :none)
-savefig(signal_plt, "test/paperfigs/SunFlower_barbara_ftrouser.png")
+## Fig. 6(b) barbara eye graph signal
+f = matread(joinpath(@__DIR__, "..", "datasets", "sunflower_barbara_voronoi.mat"))["f_eye_voronoi"]
+scatter_gplot(X; marker = f, ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); signal_plt = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
+savefig(signal_plt, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_feye.png"))
 
 ## Fig. 6(c) barbara eye relative l2 approximation error by various methods
-DVEC = signal_transform_coeff(f, ht_elist_dual, ht_elist_varimax, wavelet_packet_dual, wavelet_packet_varimax, 𝛷, W, X)
-approx_error_plot2(DVEC); plot!(legend = :topright); approx_error_plt = current()
-savefig(approx_error_plt, "test/paperfigs/SunFlower_barbara_ftrouser_DAG_approx.png")
+DVEC = signal_transform_coeff(f, ht_elist_PC, ht_elist_VM, wavelet_packet_PC, wavelet_packet_VM, 𝚽, W, X)
+approx_error_plot2(DVEC); approx_error_plt = plot!(legend = :topright, xguidefontsize=16, yguidefontsize=16, legendfontsize=12)
+savefig(approx_error_plt, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_feye_DAG_approx.png"))
 
-## Fig. 7 barbara eye 9 most important PC-NGWP vectors (ignore the DC vector)
-parent_dual = HTree_findParent(ht_elist_dual); Wav_dual = best_basis_selection(f, wavelet_packet_dual, parent_dual); dvec_spectral = Wav_dual' * f
-importance_idx = sortperm(abs.(dvec_spectral), rev = true)
+## Fig. 7 barbara eye 9 most important VM-NGWP vectors (ignore the DC vector)
+parent_VM = HTree_findParent(ht_elist_VM); Wav_VM = best_basis_selection(f, wavelet_packet_VM, parent_VM); dvec_VM = Wav_VM' * f
+importance_idx = sortperm(abs.(dvec_VM), rev = true)
 for i = 2:10
-    w = Wav_dual[:,importance_idx[i]]
-    lm_sgn = (maximum(w) > -minimum(w)) * 2 - 1
-    scatter_gplot(X; marker = lm_sgn .* w, ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); important_NGW_basis_vectors = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], yflip = true, frame = :none)
-    savefig(important_NGW_basis_vectors, "test/paperfigs/SunFlower_barbara_ftrouser_DAG_PC_NGW_important_basis_vector$(i).png")
+    scatter_gplot(X; marker = Wav_VM[:,importance_idx[i]], ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); important_NGW_basis_vectors = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
+    savefig(important_NGW_basis_vectors, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_feye_DAG_VM_NGW_important_basis_vector$(i).png"))
+end
+
+## Fig. 8(b) barbara pants graph signal
+f = matread(joinpath(@__DIR__, "..", "datasets", "sunflower_barbara_voronoi.mat"))["f_trouser_voronoi"]
+scatter_gplot(X; marker = f, ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); signal_plt = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
+savefig(signal_plt, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_ftrouser.png"))
+
+## Fig. 8(c) barbara eye relative l2 approximation error by various methods
+DVEC = signal_transform_coeff(f, ht_elist_PC, ht_elist_VM, wavelet_packet_PC, wavelet_packet_VM, 𝚽, W, X)
+approx_error_plot2(DVEC); approx_error_plt = plot!(legend = :topright, xguidefontsize=16, yguidefontsize=16, legendfontsize=12)
+savefig(approx_error_plt, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_ftrouser_DAG_approx.png"))
+
+## Fig. 9 barbara eye 9 most important PC-NGWP vectors (ignore the DC vector)
+parent_PC = HTree_findParent(ht_elist_PC); Wav_PC = best_basis_selection(f, wavelet_packet_PC, parent_PC); dvec_PC = Wav_PC' * f
+importance_idx = sortperm(abs.(dvec_PC), rev = true)
+for i = 2:10
+    w = Wav_PC[:,importance_idx[i]]
+    sgn = (maximum(w) > -minimum(w)) * 2 - 1
+    scatter_gplot(X; marker = sgn .* w, ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); important_NGW_basis_vectors = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
+    savefig(important_NGW_basis_vectors, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_ftrouser_DAG_PC_NGW_important_basis_vector$(i).png"))
 end
--- test/paperscript/Toronto_approx_exp.jl
+++ test/paperscript/Toronto_approx_exp.jl
@@ -1,7 +1,4 @@
 ## Set up environment and load required packages
-using Pkg
-Pkg.activate(".")
-
 using NGWP, JLD, MAT, Plots, LightGraphs, Distances, MTSG
 gr(dpi = 200)
 
@@ -12,62 +9,80 @@ W = 1.0 .* adjacency_matrix(G)
 dist_X = pairwise(Euclidean(),X; dims = 1)
 Weight = W .* dualGraph(dist_X; method = "inverse") # weighted adjacence matrix
 L = Matrix(Diagonal(sum(Weight;dims = 1)[:]) - Weight)
-lamb, 𝛷 = eigen(L); sgn = (maximum(𝛷, dims = 1)[:] .> -minimum(𝛷, dims = 1)[:]) .* 2 .- 1; 𝛷 = Matrix((𝛷' .* sgn)')
+𝛌, 𝚽 = eigen(L); sgn = (maximum(𝚽, dims = 1)[:] .> -minimum(𝚽, dims = 1)[:]) .* 2 .- 1; 𝚽 = 𝚽 .* sgn';
 Q = incidence_matrix(G; oriented = true)
 edge_weight = 1 ./ sqrt.(sum((Q' * X).^2, dims = 2)[:])
 
 ## Build Dual Graph by DAG metric
-distDAG = eigDAG_Distance(𝛷,Q,N; edge_weight = edge_weight)
+distDAG = eigDAG_Distance(𝚽,Q,N; edge_weight = edge_weight)
 W_dual = sparse(dualGraph(distDAG)) #required: sparse dual weighted adjacence matrix
 
 ## Assemble natural graph wavelet packets
-ht_elist_dual, ht_vlist_dual = HTree_EVlist(𝛷,W_dual)
-wavelet_packet_dual = HTree_wavelet_packet(𝛷,ht_vlist_dual,ht_elist_dual)
-ht_elist_varimax = ht_elist_dual
-wavelet_packet_varimax = HTree_wavelet_packet_varimax(𝛷,ht_elist_varimax)
+ht_elist_PC, ht_vlist_PC = HTree_EVlist(𝚽,W_dual)
+wavelet_packet_PC = HTree_wavelet_packet(𝚽,ht_vlist_PC,ht_elist_PC)
+ht_elist_VM = ht_elist_PC
+wavelet_packet_VM = HTree_wavelet_packet_varimax(𝚽,ht_elist_VM)
 
-## Fig. 8(a) a smooth spatial distribution of the street intersections graph signal
+## Fig. 10(a) a smooth spatial distribution of the street intersections graph signal
 f = zeros(N); for i in 1:N; f[i] = length(findall(dist_X[:,i] .< 1/minimum(edge_weight))); end #fneighbor
 gplot(W, X; width=1); scatter_gplot!(X; marker = f, smallValFirst = true, ms = 3); signal_plt = plot!(aspect_ratio = 1, grid = false)
-savefig(signal_plt, "test/paperfigs/Toronto_fdensity.png")
+savefig(signal_plt, joinpath(@__DIR__, "../paperfigs/Toronto_fdensity.png"))
 
-## Fig. 8(b) spatial distribution signal relative l2 approximation error by various methods
-DVEC = signal_transform_coeff(f, ht_elist_dual, ht_elist_varimax, wavelet_packet_dual, wavelet_packet_varimax, 𝛷, W, X)
-approx_error_plot2(DVEC); plot!(legend = :topright); approx_error_plt = current()
-savefig(approx_error_plt, "test/paperfigs/Toronto_fdensity_DAG_approx.png")
+## Fig. 10(b) spatial distribution signal relative l2 approximation error by various methods
+DVEC = signal_transform_coeff(f, ht_elist_PC, ht_elist_VM, wavelet_packet_PC, wavelet_packet_VM, 𝚽, W, X)
+approx_error_plot2(DVEC); approx_error_plt = plot!(legend = :topright, xguidefontsize=14, yguidefontsize=14, legendfontsize=10)
+savefig(approx_error_plt, joinpath(@__DIR__, "../paperfigs/Toronto_fdensity_DAG_approx.png"))
 
-## Fig. 9 spatial distribution signal 9 most important VM-NGWP vectors (ignore the DC vector)
-parent_varimax = HTree_findParent(ht_elist_varimax); Wav_varimax = best_basis_selection(f, wavelet_packet_varimax, parent_varimax); dvec_varimax = Wav_varimax' * f
-importance_idx = sortperm(abs.(dvec_varimax), rev = true)
+## Fig. 11 spatial distribution signal 9 most important VM-NGWP vectors (ignore the DC vector)
+parent_VM = HTree_findParent(ht_elist_VM); Wav_VM = best_basis_selection(f, wavelet_packet_VM, parent_VM); dvec_VM = Wav_VM' * f
+importance_idx = sortperm(abs.(dvec_VM), rev = true)
 for i = 2:10
-    gplot(W, X; width=1); scatter_gplot!(X; marker = Wav_varimax[:,importance_idx[i]], smallValFirst = true, ms = 3); important_NGW_basis_vectors = plot!(grid = false)
-    savefig(important_NGW_basis_vectors, "test/paperfigs/Toronto_fdensity_DAG_VM_NGW_important_basis_vector$(i).png")
+    gplot(W, X; width=1); scatter_gplot!(X; marker = Wav_VM[:,importance_idx[i]], smallValFirst = true, ms = 3); important_NGW_basis_vectors = plot!(grid = false)
+    savefig(important_NGW_basis_vectors, joinpath(@__DIR__, "../paperfigs/Toronto_fdensity_DAG_VM_NGW_important_basis_vector$(i).png"))
 end
 
-## Fig. 10(a) pedestrian volume graph signal
+## Show some important HGLET vectors
+using MTSG
+tmp=zeros(length(f),1); tmp[:,1]=f; G_Sig=GraphSig(1.0*W, xy=X, f=tmp)
+G_Sig = Adj2InvEuc(G_Sig); GP = partition_tree_fiedler(G_Sig,:Lrw)
+dmatrixH, dmatrixHrw, dmatrixHsym = HGLET_Analysis_All(G_Sig, GP) # expansion coefficients of 3-way HGLET bases
+dvec_hglet, BS_hglet, trans_hglet = HGLET_GHWT_BestBasis(GP, dmatrixH = dmatrixH, dmatrixHrw = dmatrixHrw, dmatrixHsym = dmatrixHsym, costfun = 1) # best-basis among all combinations of bases
+importance_idx = sortperm(dvec_hglet.^2; rev = true)[1:10]
+for i = 2:10
+    important_HGLET_basis_vector, _ = HGLET_Synthesis(reshape(spike(importance_idx[i],N), N, 1), GP, BS_hglet, G_Sig)
+    w = important_HGLET_basis_vector[:,1]
+    sgn = (maximum(w) > -minimum(w)) * 2 - 1
+    gplot(W, X; width=1); scatter_gplot!(X; marker = sgn .* w, smallValFirst = true, ms = 3); important_HGLET_basis_vectors = plot!(grid = false)
+    savefig(important_HGLET_basis_vectors, joinpath(@__DIR__, "../paperfigs/Toronto_fdensity_HGLET_important_basis_vector$(i).png"))
+end
+
+for i = 2:10
+    println("(j, k, l) = ", HGLET_jkl(GP, BS_hglet.levlist[importance_idx[i]][1], BS_hglet.levlist[importance_idx[i]][2]))
+end
+
+## Fig. 12(a) pedestrian volume graph signal
 fp = load(joinpath(@__DIR__, "..", "datasets", "new_toronto.jld"),"fp")
 gplot(W, X; width=1); scatter_gplot!(X; marker = fp, smallValFirst = true, ms = 3); signal_plt = plot!(aspect_ratio = 1, grid = false)
-savefig(signal_plt, "test/paperfigs/Toronto_fp.png")
+savefig(signal_plt, joinpath(@__DIR__, "../paperfigs/Toronto_fp.png"))
 
-## Fig. 10(b) pedestrian signal relative l2 approximation error by various methods
-DVEC = signal_transform_coeff(fp, ht_elist_dual, ht_elist_varimax, wavelet_packet_dual, wavelet_packet_varimax, 𝛷, W, X)
-approx_error_plot2(DVEC); plot!(legend = :topright); approx_error_plt = current()
-savefig(approx_error_plt, "test/paperfigs/Toronto_fp_DAG_approx.png")
+## Fig. 12(b) pedestrian signal relative l2 approximation error by various methods
+DVEC = signal_transform_coeff(fp, ht_elist_PC, ht_elist_VM, wavelet_packet_PC, wavelet_packet_VM, 𝚽, W, X)
+approx_error_plot2(DVEC); approx_error_plt = plot!(legend = :topright, xguidefontsize=14, yguidefontsize=14, legendfontsize=10)
+savefig(approx_error_plt, joinpath(@__DIR__, "../paperfigs/Toronto_fp_DAG_approx.png"))
 
-## Fig. 11 barbara eye 9 most important PC-NGWP vectors (ignore the DC vector)
-parent_varimax = HTree_findParent(ht_elist_varimax); Wav_varimax = best_basis_selection(fp, wavelet_packet_varimax, parent_varimax); dvec_varimax = Wav_varimax' * fp
-importance_idx = sortperm(abs.(dvec_varimax), rev = true)
+## Fig. 13 pedestrian signal 9 most important VM-NGWP vectors (ignore the DC vector)
+parent_VM = HTree_findParent(ht_elist_VM); Wav_VM = best_basis_selection(fp, wavelet_packet_VM, parent_VM); dvec_VM = Wav_VM' * fp
+importance_idx = sortperm(abs.(dvec_VM), rev = true)
 for i = 2:10
-    gplot(W, X; width=1); scatter_gplot!(X; marker = Wav_varimax[:,importance_idx[i]], smallValFirst = true, ms = 3); important_NGW_basis_vectors = plot!(grid = false)
-    savefig(important_NGW_basis_vectors, "test/paperfigs/Toronto_fp_DAG_VM_NGW_important_basis_vector$(i).png")
+    gplot(W, X; width=1); scatter_gplot!(X; marker = Wav_VM[:,importance_idx[i]], smallValFirst = true, ms = 3); important_NGW_basis_vectors = plot!(grid = false)
+    savefig(important_NGW_basis_vectors, joinpath(@__DIR__, "../paperfigs/Toronto_fp_DAG_VM_NGW_important_basis_vector$(i).png"))
 end
 
-## Fig. 12 barbara eye 9 most important PC-NGWP vectors (ignore the DC vector)
-parent_dual = HTree_findParent(ht_elist_dual); Wav_dual = best_basis_selection(fp, wavelet_packet_dual, parent_dual); dvec_pc = Wav_dual' * fp
-importance_idx = sortperm(abs.(dvec_pc), rev = true)
+parent_PC = HTree_findParent(ht_elist_PC); Wav_PC = best_basis_selection(fp, wavelet_packet_PC, parent_PC); dvec_PC = Wav_PC' * fp
+importance_idx = sortperm(abs.(dvec_PC), rev = true)
 for i = 2:10
-    w = Wav_dual[:,importance_idx[i]]
-    lm_sgn = (maximum(w) > -minimum(w)) * 2 - 1
-    gplot(W, X; width=1); scatter_gplot!(X; marker = lm_sgn .* w, smallValFirst = true, ms = 3); important_NGW_basis_vectors = plot!(grid = false)
-    savefig(important_NGW_basis_vectors, "test/paperfigs/Toronto_fp_DAG_PC_NGW_important_basis_vector$(i).png")
+    w = Wav_PC[:,importance_idx[i]]
+    sgn = (maximum(w) > -minimum(w)) * 2 - 1
+    gplot(W, X; width=1); scatter_gplot!(X; marker = sgn .* w, smallValFirst = true, ms = 3); important_NGW_basis_vectors = plot!(grid = false)
+    savefig(important_NGW_basis_vectors, joinpath(@__DIR__, "../paperfigs/Toronto_fp_DAG_PC_NGW_important_basis_vector$(i).png"))
 end
