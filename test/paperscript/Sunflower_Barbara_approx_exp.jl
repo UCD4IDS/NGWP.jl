23c6481bc04dd4417ca9dad024cc7abced8c6eb3
haotian127 <htsli@ucdavis.edu>
2021-02-07 15:18:56 -0800

    update Sunflower approx exp script and corresponding figs.

--- test/paperscript/Sunflower_Barbara_approx_exp.jl
+++ test/paperscript/Sunflower_Barbara_approx_exp.jl
@@ -1,4 +1,5 @@
-## Set up environment and load required packages
+# script for Fig.8, Fig.9, Fig.10, Fig.11
+
 using NGWP, JLD, MAT, Plots, LightGraphs, MTSG
 gr(dpi = 200)
 
@@ -19,40 +20,38 @@ wavelet_packet_PC = HTree_wavelet_packet(𝚽,ht_vlist_PC,ht_elist_PC)
 ht_elist_VM = ht_elist_PC
 wavelet_packet_VM = HTree_wavelet_packet_varimax(𝚽,ht_elist_VM)
 
-## Fig. 6(b) barbara eye graph signal
+#################### Fig. 8(b) barbara eye graph signal
 f = matread(joinpath(@__DIR__, "..", "datasets", "sunflower_barbara_voronoi.mat"))["f_eye_voronoi"]
-scatter_gplot(X; marker = f, ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); signal_plt = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
+scatter_gplot(X; marker = f, ms = LinRange(4.0, 14.0, N), c = :greys); signal_plt = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
 savefig(signal_plt, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_feye.png"))
 
-## Fig. 6(c) barbara eye relative l2 approximation error by various methods
+#################### Fig. 8(c) barbara eye relative l2 approximation error by various methods
 DVEC = signal_transform_coeff(f, ht_elist_PC, ht_elist_VM, wavelet_packet_PC, wavelet_packet_VM, 𝚽, W, X)
 approx_error_plot2(DVEC); approx_error_plt = plot!(legend = :topright, xguidefontsize=16, yguidefontsize=16, legendfontsize=12)
 savefig(approx_error_plt, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_feye_DAG_approx.png"))
 
-## Fig. 7 barbara eye 9 most important VM-NGWP vectors (ignore the DC vector)
+#################### Fig. 9 barbara eye 16 most important VM-NGWP vectors (ignore the DC vector)
 parent_VM = HTree_findParent(ht_elist_VM); Wav_VM = best_basis_selection(f, wavelet_packet_VM, parent_VM); dvec_VM = Wav_VM' * f
 importance_idx = sortperm(abs.(dvec_VM), rev = true)
-for i = 2:10
-    scatter_gplot(X; marker = Wav_VM[:,importance_idx[i]], ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); important_NGW_basis_vectors = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
-    savefig(important_NGW_basis_vectors, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_feye_DAG_VM_NGW_important_basis_vector$(i).png"))
+for i = 2:17
+    scatter_gplot(X; marker = Wav_VM[:,importance_idx[i]], ms = LinRange(4.0, 14.0, N), c = :greys); important_NGW_basis_vectors = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none, cbar = false, clims = (-0.15,0.15))
+    savefig(important_NGW_basis_vectors, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_feye_DAG_VM_NGW_important_basis_vector$(lpad(i,2,"0")).png"))
 end
 
-## Fig. 8(b) barbara pants graph signal
+#################### Fig. 10(b) barbara pants graph signal
 f = matread(joinpath(@__DIR__, "..", "datasets", "sunflower_barbara_voronoi.mat"))["f_trouser_voronoi"]
-scatter_gplot(X; marker = f, ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); signal_plt = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
+scatter_gplot(X; marker = f, ms = LinRange(4.0, 14.0, N), c = :greys); signal_plt = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
 savefig(signal_plt, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_ftrouser.png"))
 
-## Fig. 8(c) barbara eye relative l2 approximation error by various methods
+#################### Fig. 10(c) barbara eye relative l2 approximation error by various methods
 DVEC = signal_transform_coeff(f, ht_elist_PC, ht_elist_VM, wavelet_packet_PC, wavelet_packet_VM, 𝚽, W, X)
 approx_error_plot2(DVEC); approx_error_plt = plot!(legend = :topright, xguidefontsize=16, yguidefontsize=16, legendfontsize=12)
 savefig(approx_error_plt, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_ftrouser_DAG_approx.png"))
 
-## Fig. 9 barbara eye 9 most important PC-NGWP vectors (ignore the DC vector)
-parent_PC = HTree_findParent(ht_elist_PC); Wav_PC = best_basis_selection(f, wavelet_packet_PC, parent_PC); dvec_PC = Wav_PC' * f
-importance_idx = sortperm(abs.(dvec_PC), rev = true)
-for i = 2:10
-    w = Wav_PC[:,importance_idx[i]]
-    sgn = (maximum(w) > -minimum(w)) * 2 - 1
-    scatter_gplot(X; marker = sgn .* w, ms = LinRange(4.0, 14.0, N), smallValFirst = false, c = :greys); important_NGW_basis_vectors = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none)
-    savefig(important_NGW_basis_vectors, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_ftrouser_DAG_PC_NGW_important_basis_vector$(i).png"))
+#################### Fig. 11 barbara pants 16 most important VM-NGWP vectors (ignore the DC vector)
+parent_VM = HTree_findParent(ht_elist_VM); Wav_VM = best_basis_selection(f, wavelet_packet_VM, parent_VM); dvec_VM = Wav_VM' * f
+importance_idx = sortperm(abs.(dvec_VM), rev = true)
+for i = 2:17
+    scatter_gplot(X; marker = Wav_VM[:,importance_idx[i]], ms = LinRange(4.0, 14.0, N), c = :greys); important_NGW_basis_vectors = plot!(xlim = [-1.2,1.2], ylim = [-1.2,1.2], frame = :none, cbar = false, clims = (-0.15,0.15))
+    savefig(important_NGW_basis_vectors, joinpath(@__DIR__, "../paperfigs/SunFlower_barbara_ftrouser_DAG_VM_NGW_important_basis_vector$(lpad(i,2,"0")).png"))
 end
