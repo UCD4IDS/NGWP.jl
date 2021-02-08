069dd6bfa275c5d5b77c8550010fdd1f93e7b2ed
haotian127 <htsli@ucdavis.edu>
2021-02-07 16:59:04 -0800

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,35 +7,32 @@ using Test, JLD, MAT, Plots, LightGraphs, LinearAlgebra, SparseArrays
 #####################################################
 println("Testing NGWP on sunflower barbara eye signal")
 ## Build weighted sunflower graph for test
-G, L, X = SunFlowerGraph(N = 400); N = nv(G)
-lamb, 𝛷 = eigen(Matrix(L)); sgn = (maximum(𝛷, dims = 1)[:] .> -minimum(𝛷, dims = 1)[:]) .* 2 .- 1; 𝛷 = Matrix((𝛷' .* sgn)')
+G, L, X = SunFlowerGraph(); N = nv(G)
+𝛌, 𝚽 = eigen(Matrix(L)); sgn = (maximum(𝚽, dims = 1)[:] .> -minimum(𝚽, dims = 1)[:]) .* 2 .- 1; 𝚽 = 𝚽 * Diagonal(sgn);
 Q = incidence_matrix(G; oriented = true)
 W = 1.0 * adjacency_matrix(G)
 edge_weight = [e.weight for e in edges(G)]
 
 ## Build Dual Graph by DAG metric
-distDAG = eigDAG_Distance(𝛷,Q,N; edge_weight = edge_weight)
+distDAG = eigDAG_Distance(𝚽,Q,N; edge_weight = edge_weight)
 W_dual = sparse(dualGraph(distDAG)) #required: sparse dual weighted adjacence matrix
 
 ## Assemble natural graph wavelet packets
-ht_elist_dual, ht_vlist_dual = HTree_EVlist(𝛷,W_dual)
-wavelet_packet_dual = HTree_wavelet_packet(𝛷,ht_vlist_dual,ht_elist_dual)
-ht_elist_varimax = ht_elist_dual
-wavelet_packet_varimax = HTree_wavelet_packet_varimax(𝛷,ht_elist_varimax)
+ht_elist_PC, ht_vlist_PC = HTree_EVlist(𝚽,W_dual)
+wavelet_packet_PC = HTree_wavelet_packet(𝚽,ht_vlist_PC,ht_elist_PC)
+ht_elist_VM = ht_elist_PC
+wavelet_packet_VM = HTree_wavelet_packet_varimax(𝚽,ht_elist_VM)
 
 ## use barbara eye signal for testing
-f = matread(joinpath(@__DIR__, "datasets", "sunflower_barbara.mat"))["f_eye"]
+f = matread(joinpath(@__DIR__, "datasets", "sunflower_barbara_voronoi.mat"))["f_eye_voronoi"]
 
-DVEC = signal_transform_coeff(f, ht_elist_dual, ht_elist_varimax, wavelet_packet_dual, wavelet_packet_varimax, 𝛷, W, X)
-
+DVEC = signal_transform_coeff(f, ht_elist_PC, ht_elist_VM, wavelet_packet_PC, wavelet_packet_VM, 𝚽, W, X)
 DVEC_truth = JLD.load(joinpath(@__DIR__, "datasets", "sunflower_barbara_feye_DVEC.jld"), "DVEC")
-dvec_PC_NGWP_truth = DVEC_truth[7]
-dvec_VM_NGWP_truth = DVEC_truth[8]
 
 @testset "PC-NGWP Test" begin
-    @test norm(DVEC[8]-dvec_PC_NGWP_truth)/norm(dvec_PC_NGWP_truth) < 1e-8
+    @test norm(DVEC[end-1]-DVEC_truth[end-1])/norm(DVEC_truth[end-1]) < 1e-8
 end
 
 @testset "VM-NGWP Test" begin
-    @test norm(DVEC[9]-dvec_VM_NGWP_truth)/norm(dvec_VM_NGWP_truth) < 1e-8
+    @test norm(DVEC[end]-DVEC_truth[end])/norm(DVEC_truth[end]) < 1e-8
 end
