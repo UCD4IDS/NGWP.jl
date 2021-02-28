cac370a25db537cf4f906288acb49d5e2fc5926b
haotian127 <htsli@ucdavis.edu>
2021-02-27 20:58:22 -0800

    export TSD distance

--- src/NGWP.jl
+++ src/NGWP.jl
@@ -19,6 +19,7 @@ end
 
 export eigDAG_Distance, eigHAD_Distance, eigHAD_Affinity
 export eigROT_Distance, ROT_Distance, eigEMD_Distance
+export eigTSD_Distance, K_functional
 export SunFlowerGraph, dualgraph
 export pc_ngwp, pairclustering
 export vm_ngwp
