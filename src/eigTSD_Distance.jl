00e3d09d5e411831d82a8dc151a16eca52ec57c6
haotian127 <htsli@ucdavis.edu>
2021-03-02 10:05:24 -0800

    get rid of unused variable `L`

--- src/eigTSD_Distance.jl
+++ src/eigTSD_Distance.jl
@@ -1,7 +1,7 @@
 """
     eigTSD_Distance(P::Matrix{Float64}, 𝚽::Matrix{Float64}, 𝛌::Vector{Float64},
-                        Q::Matrix{Float64}, L::Matrix{Float64}; length::Any = 1,
-                        T::Any = :Inf, dt::Float64 = 0.1, tol::Float64 = 1e-5)
+                    Q::SparseMatrixCSC{Int64,Int64}; length::Any = 1,
+                    T::Any = :Inf, dt::Float64 = 0.1, tol::Float64 = 1e-5)
 
 computes the TSD distance matrix of P's column vectors on a graph.
 
@@ -10,10 +10,9 @@ computes the TSD distance matrix of P's column vectors on a graph.
 - `𝚽::Matrix{Float64}`: matrix of the unweighted graph Laplacian eigenvectors.
 - `𝛌::Vector{Float64}`: vector of eigenvalues.
 - `Q::SparseMatrixCSC{Int64,Int64}`: the unweighted incidence matrix.
-- `L::Matrix{Float64}`: the unweighted graph Laplacian matrix.
 - `length::Any`: vector of edge lengths (default: `1` represents unweighted graphs)
 - `T::Any`: the stopping time T in K_functional (default: `:Inf`)
-- `dt::Float64`: time increment (default: `0.1`)
+- `dt::Float64`: the time increment in the integral of K_functional (default: `0.1`)
 - `tol::Float64`: tolerance for convergence (default: `1e-5`)
 
 # Output Argument
@@ -21,8 +20,8 @@ computes the TSD distance matrix of P's column vectors on a graph.
 
 """
 function eigTSD_Distance(P::Matrix{Float64}, 𝚽::Matrix{Float64}, 𝛌::Vector{Float64},
-                            Q::SparseMatrixCSC{Int64,Int64}, L::Matrix{Int}; length::Any = 1,
-                            T::Any = :Inf, dt::Float64 = 0.1, tol::Float64 = 1e-5)
+                         Q::SparseMatrixCSC{Int64,Int64}; length::Any = 1,
+                         T::Any = :Inf, dt::Float64 = 0.1, tol::Float64 = 1e-5)
     N, ncols = Base.size(P)
     total_mass = sum(P, dims = 1)[:]
     if norm(total_mass - total_mass[1] * ones(ncols), Inf) > 10^4 * eps()
@@ -35,7 +34,7 @@ function eigTSD_Distance(P::Matrix{Float64}, 𝚽::Matrix{Float64}, 𝛌::Vector
     ∇𝚽 = Q' * 𝚽
 
     for i = 1:(ncols - 1), j = (i + 1):ncols
-        dis[i, j] = K_functional(P[:, i], P[:, j], 𝚽, ∇𝚽, 𝛌, L; length = length,
+        dis[i, j] = K_functional(P[:, i], P[:, j], 𝚽, ∇𝚽, 𝛌; length = length,
                                     T = T, dt = dt, tol = tol)[1]
     end
     return dis + dis'
@@ -43,9 +42,8 @@ end
 
 """
     K_functional(𝐩::Vector{Float64}, 𝐪::Vector{Float64}, 𝚽::Matrix{Float64},
-                        ∇𝚽::Matrix{Float64}, 𝛌::Vector{Float64}, L::Matrix{Int};
-                        length::Any = 1, T::Any = :Inf, dt::Float64 = 0.1,
-                        tol::Float64 = 1e-5)
+                 ∇𝚽::Matrix{Float64}, 𝛌::Vector{Float64}; length::Any = 1,
+                 T::Any = :Inf, dt::Float64 = 0.1, tol::Float64 = 1e-5)
 
 computes the K_functional between two vector meassures 𝐩 and 𝐪 on a graph.
 
@@ -55,7 +53,6 @@ computes the K_functional between two vector meassures 𝐩 and 𝐪 on a graph.
 - `𝚽::Matrix{Float64}`: matrix of the unweighted graph Laplacian eigenvectors.
 - `∇𝚽::Matrix{Float64}`: gradient of unweighted graph Laplacian eigenvectors.
 - `𝛌::Vector{Float64}`: vector of eigenvalues.
-- `L::Matrix{Int}`: the unweighted graph Laplacian matrix.
 - `length::Any`: vector of edge lengths (default: 1 represents unweighted graphs)
 - `T::Any`: the stopping time T in K_functional (default: :Inf)
 - `dt::Float64`: time increment (default: 0.1)
@@ -67,9 +64,8 @@ computes the K_functional between two vector meassures 𝐩 and 𝐪 on a graph.
 
 """
 function K_functional(𝐩::Vector{Float64}, 𝐪::Vector{Float64}, 𝚽::Matrix{Float64},
-                        ∇𝚽::Matrix{Float64}, 𝛌::Vector{Float64}, L::Matrix{Int};
-                        length::Any = 1, T::Any = :Inf, dt::Float64 = 0.1,
-                        tol::Float64 = 1e-5)
+                        ∇𝚽::Matrix{Float64}, 𝛌::Vector{Float64}; length::Any = 1,
+                        T::Any = :Inf, dt::Float64 = 0.1, tol::Float64 = 1e-5)
     if abs(sum(𝐩 - 𝐪)) > 10^4 * eps()
         @error("𝐩 and 𝐪 do not have the same total mass.")
     end
