4ee28a06fbb942bbd01f4564cd48743aa119832c
haotian127 <htsli@ucdavis.edu>
2021-04-06 11:07:46 -0700

    add Chapter 3 figure scripts

---
+++ dissertation/scripts/setups/rgc100.jl
@@ -0,0 +1,10 @@
+using Plots, LightGraphs, JLD, LaTeXStrings, MTSG, NGWP
+using Plots.PlotMeasures
+
+G = loadgraph("../datasets/RGC100.lgz"); N = nv(G)
+X = load("../datasets/RGC100_xyz.jld", "xyz")[:, 1:2]
+X3 = load("../datasets/RGC100_xyz.jld", "xyz")
+L = Matrix(laplacian_matrix(G))
+𝛌, 𝚽 = eigen(L)
+standardize_eigenvectors!(𝚽)
+W = 1.0 * adjacency_matrix(G)
