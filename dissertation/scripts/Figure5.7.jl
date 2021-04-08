861727c9c915e36dd4d84877d6297fbd37ca4028
haotian127 <htsli@ucdavis.edu>
2021-04-08 15:52:27 -0700

    update gif

--- dissertation/scripts/Figure5.7.jl
+++ dissertation/scripts/Figure5.7.jl
@@ -16,3 +16,15 @@ plt = grid7x3_mds_heatmaps(E, 𝚽; backend = :pyplot,
             annotate_ind = vcat(1:8, 11), plotOrder = vcat(N:-1:9, 6:8, 5:-1:1))
 savefig(plt, "../figs/Grid7x3_MDS_TSD_Tinfty.png")
 display(plt)
+
+
+## TSD T from 0.1 to 10
+anim = @animate for t ∈ 0.1:0.1:10
+    D = natural_eigdist(𝚽, 𝛌, Q; T = t, distance = :TSD)
+    E = transform(fit(MDS, D, maxoutdim=2, distances=true))
+    E[1, :] .*= sign(E[1, 2])
+    E[2, :] .*= sign(E[2, 2]) * ((t <= 1) * 2 - 1)
+    grid7x3_mds_heatmaps(E, 𝚽; backend = :pyplot)
+    title!("T = $(t)")
+end
+gif(anim, "../gifs/Grid7x3_MDS_TSD_T01_inf.gif", fps = 5)
