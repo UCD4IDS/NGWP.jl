9c0ce70ba6465fd81829730e5413d5dac1341c59
haotian127 <htsli@ucdavis.edu>
2021-04-08 21:18:03 -0700

    minor bug fix

--- dissertation/scripts/Figure5.12.jl
+++ dissertation/scripts/Figure5.12.jl
@@ -2,7 +2,7 @@ cd(@__DIR__); include("setups/simpletree.jl");
 plotlyjs(dpi = 200)
 
 ## (a)
-D = natural_eigdist(𝚽, 𝛌, Q; α = 1.0, input_format = :pmf2, distance = :ROT)
+D = natural_eigdist(𝚽, 𝛌, Q; α = 1.0, input_format = :pmf1, distance = :ROT)
 E = transform(fit(MDS, D, maxoutdim=3, distances=true))
 E[1, :] .*= 1; E[2, :] .*= -1; E[3, :] .*= 1;
 plt = simpletree_mds_plot(E, ieb1, ieb2, ieb3, ieb4, iejc); plot!(camera=(210,-37), xlims = [-18, 15])
